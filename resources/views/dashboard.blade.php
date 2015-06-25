1b204a39cda80ae038a7cb8e601100fb4f6a1125
peter <binmonk@gmail.com>
2015-06-25 16:30:09 +0300

    things! overlays and stuff

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -67,9 +67,40 @@
     <!-- /.content-wrapper -->
 
     <!-- Footer -->
+
+    <input type="hidden" name="_token" id="CSRFtoken" value="{{ csrf_token() }}"/>
     @include('footer')
     @include('right')
 
+</div>
+<div class="overlay overlay-hugeinc">
+    <button type="button" class="overlay-close">
+        <i class="fa fa-times fa-lg"></i>
+    </button>
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3" style="">
+            <form role="form">
+               <span class="input input--isao">
+					<input class="input__field input__field--isao" type="text" id="input-38"/>
+					<label class="input__label input__label--isao" for="input-38" data-content="First Name">
+                        <span class="input__label-content input__label-content--isao">First Name</span>
+                    </label>
+				</span>
+				<span class="input input--isao">
+					<input class="input__field input__field--isao title-editable my-editable-divs" type="text"
+                           id="input-39"/>
+					<label class="input__label input__label--isao" for="input-39" data-content="Middle Name">
+                        <span class="input__label-content input__label-content--isao">Middle Name</span>
+                    </label>
+				</span>
+
+                <div class="description-editable my-editable-divs" id="description">
+                    <p>Enter post body</p>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
 </div>
 <!-- ./wrapper -->
 
@@ -89,6 +120,7 @@
     buttonLabels: 'fontawesome'
     // initializing editors
     var titleEditor = new MediumEditor('.title-editable');
+    var descriptionEditor = new MediumEditor('.description-editable ');
     var bodyEditor = new MediumEditor('.body-editable');
     $(function () {
         // initializing insert image on body editor
