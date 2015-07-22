db8fd0c7795b86df48984ffbe334ec9c30eeb91b
peter <binmonk@gmail.com>
2015-07-22 15:37:28 +0300

    add sign in page

---
+++ resources/views/auth/signin.blade.php
@@ -0,0 +1,30 @@
+@extends('frontend')
+
+@section('content')
+<style>
+.outer-div{
+     padding: 20%;
+     min-height: 100%;
+     overflow-y: hidden;
+}
+::-webkit-scrollbar { 
+    display: none; 
+}
+.inner-div{
+     margin: 0 auto;
+     width: 100px; 
+}
+</style>
+	<div class="outer-div blue accent-3">
+  		<div class="inner-div">
+  			  <a href="/auth/github" class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">settings_power</i></a>
+  		</div>
+	</div>
+@endsection
+@section('footer-content')
+	 <script>
+    $( document ).ready(function(){
+         Materialize.toast("Click the button below to log in.", 4000, 'rounded');
+    })
+</script>
+@endsection
