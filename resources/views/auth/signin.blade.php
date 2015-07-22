f6b0c5fc2d4bf0900ea99b1fd09e617cf8bdc2f7
peter <binmonk@gmail.com>
2015-07-22 16:10:08 +0300

    slight tweaks to the login page

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -3,15 +3,26 @@
 @section('content')
 <style>
 .outer-div{
-     padding: 20%;
-     min-height: 2000px;
-     overflow-y: hidden;
+    /*padding: 20%;*/
+    min-height: 100%;
+    min-width: 100%;
+    position: absolute;
+    top: 0;
+    width: auto;
 }
 ::-webkit-scrollbar { 
     display: none; 
 }
 .inner-div{
      margin: 0 auto;
+     text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 100px;
+    height: 100px;
+    -webkit-transform: translate(-50%, -50%);  
+    transform: translate(-50%, -50%); 
 }
 </style>
 	<div class="outer-div blue accent-3">
@@ -23,7 +34,7 @@
 @section('footer-content')
 	 <script>
     $( document ).ready(function(){
-         Materialize.toast("Click the button below to log in.", 4000, 'rounded');
+         Materialize.toast("Click the button log in.", 4000, 'rounded');
     })
 </script>
 @endsection
