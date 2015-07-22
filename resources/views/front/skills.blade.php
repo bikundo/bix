ad69efcfd6968f4d103897e19970c0049239a7dc
peter <binmonk@gmail.com>
2015-07-22 16:36:18 +0300

    close out unclosed h5s

--- resources/views/front/skills.blade.php
+++ resources/views/front/skills.blade.php
@@ -1,90 +1,91 @@
 <div class="white" id="skills-container">
-  <h5 class="light center section-headers fancy">
-    <span>Technologies I use</span>
-  </h5>
-  <div class="row light-blue darken-1">
-      <div class="col m3 s12 ">
-        <h4>Backend</h4>
-      </div>
-      <div class="col m3 s12">
-         <h5 class="center-align">PHP<h5>
-        <img src="images/svg/php.svg" width="100" class="responsive-img skill-images">
-      </div>
-      <div class="col m3 s12">
-        <h5 class="center-align">Laravel 5.1</h5>
-        <img src="images/svg/laravel.svg" width="100"  class="responsive-img skill-images">
-      </div>
-      <div class="col m3 s12">
-        <h5 class="center-align">Codeigniter</h5>
-        <img src="images/svg/codeigniter.svg" width="100" class="responsive-img skill-images">
-      </div>
-</div>
-    <div class="row  light-blue">
-     <div class="col m3 s12">
-        <h4>Frontend</h4>
-      </div>
-        <div class="col m3 s12">
-         <h5 class="center-align">CSS3<h5>
-        <img src="images/svg/css-3.svg" width="100" class="responsive-img skill-images">
+    <h5 class="light center section-headers fancy">
+        <span>Technologies I use</span>
+    </h5>
+
+    <div class="row light-blue darken-1">
+        <div class="col m3 s12 ">
+            <h4>Backend</h4>
         </div>
-      <div class="col m3 s12">
-        <h5 class="center-align">HTML5</h5>
-        <img src="images/svg/html-5.svg" width="100"  class="responsive-img skill-images">
-      </div>
-      <div class="col m3 s12">
-        <h5 class="center-align">Javascript</h5>
-        <img src="images/svg/javascript.svg" width="100" class="responsive-img skill-images">
-      </div>
+        <div class="col m3 s12">
+            <h5 class="center-align">PHP</h5>
+            <img src="images/svg/php.svg" width="100" class="responsive-img skill-images">
+        </div>
+        <div class="col m3 s12">
+            <h5 class="center-align">Laravel 5.1</h5>
+            <img src="images/svg/laravel.svg" width="100" class="responsive-img skill-images">
+        </div>
+        <div class="col m3 s12">
+            <h5 class="center-align">Codeigniter</h5>
+            <img src="images/svg/codeigniter.svg" width="100" class="responsive-img skill-images">
+        </div>
     </div>
-      <div class="row  light-blue darken-1">
-       <div class="col m3 s12">
-        <h4>Front-end Frameworks</h4>
-      </div>
+    <div class="row  light-blue">
         <div class="col m3 s12">
-         <h5 class="center-align">Bootsrap<h5>
-        <img src="images/svg/bootstrap.svg" width="100" class="responsive-img skill-images">
-      </div>
-      <div class="col m3 s12">
-        <h5 class="center-align">Foundation</h5>
-        <img src="images/svg/foundation.svg" width="100"  class="responsive-img skill-images">
-      </div>
-      <div class="col m3 s12">
-        <h5 class="center-align">VueJs</h5>
-        <img src="images/svg/vue.svg" width="100" class="responsive-img skill-images">
-      </div>
-      </div>
-      <div class="row light-blue">
-       <div class="col m3 s12">
-        <h4>Version Control</h4>
-      </div>
-        <div class="col m3 s12">
-           <h5 class="center-align">Git<h5>
-          <img src="images/svg/git.svg" width="100" class="responsive-img skill-images">
+            <h4>Frontend</h4>
         </div>
-      <div class="col m3 s12">
-        <h5 class="center-align">Bitbucket</h5>
-        <img src="images/svg/bitbucket.svg" width="100"  class="responsive-img skill-images">
-      </div>
-      <div class="col m3 s12">
-        <h5 class="center-align">Github</h5>
-        <img src="images/svg/github-icon.svg" width="100" class="responsive-img skill-images">
-      </div>
-      </div>
-      <div class="row light-blue darken-1">
-       <div class="col m3 s12">
-        <h4>Servers</h4>
-      </div>
         <div class="col m3 s12">
-           <h5 class="center-align">Aws<h5>
-          <img src="images/svg/aws.svg" width="100" class="responsive-img skill-images">
+            <h5 class="center-align">CSS3</h5>
+            <img src="images/svg/css-3.svg" width="100" class="responsive-img skill-images">
         </div>
-      <div class="col m3 s12">
-        <h5 class="center-align">Digital Ocean</h5>
-        <img src="images/svg/digitalocean.svg" width="100"  class="responsive-img skill-images">
-      </div>
-      <div class="col m3 s12">
-        <h5 class="center-align">Vagrant</h5>
-        <img src="images/svg/vagrant.svg" width="100" class="responsive-img skill-images">
-      </div>
-      </div>
-  </div>
+        <div class="col m3 s12">
+            <h5 class="center-align">HTML5</h5>
+            <img src="images/svg/html-5.svg" width="100" class="responsive-img skill-images">
+        </div>
+        <div class="col m3 s12">
+            <h5 class="center-align">Javascript</h5>
+            <img src="images/svg/javascript.svg" width="100" class="responsive-img skill-images">
+        </div>
+    </div>
+    <div class="row  light-blue darken-1">
+        <div class="col m3 s12">
+            <h4>Front-end Frameworks</h4>
+        </div>
+        <div class="col m3 s12">
+            <h5 class="center-align">Bootsrap</h5>
+            <img src="images/svg/bootstrap.svg" width="100" class="responsive-img skill-images">
+        </div>
+        <div class="col m3 s12">
+            <h5 class="center-align">Foundation</h5>
+            <img src="images/svg/foundation.svg" width="100" class="responsive-img skill-images">
+        </div>
+        <div class="col m3 s12">
+            <h5 class="center-align">VueJs</h5>
+            <img src="images/svg/vue.svg" width="100" class="responsive-img skill-images">
+        </div>
+    </div>
+    <div class="row light-blue">
+        <div class="col m3 s12">
+            <h4>Version Control</h4>
+        </div>
+        <div class="col m3 s12">
+            <h5 class="center-align">Git</h5>
+            <img src="images/svg/git.svg" width="100" class="responsive-img skill-images">
+        </div>
+        <div class="col m3 s12">
+            <h5 class="center-align">Bitbucket</h5>
+            <img src="images/svg/bitbucket.svg" width="100" class="responsive-img skill-images">
+        </div>
+        <div class="col m3 s12">
+            <h5 class="center-align">Github</h5>
+            <img src="images/svg/github-icon.svg" width="100" class="responsive-img skill-images">
+        </div>
+    </div>
+    <div class="row light-blue darken-1">
+        <div class="col m3 s12">
+            <h4>Servers</h4>
+        </div>
+        <div class="col m3 s12">
+            <h5 class="center-align">Aws</h5>
+            <img src="images/svg/aws.svg" width="100" class="responsive-img skill-images">
+        </div>
+        <div class="col m3 s12">
+            <h5 class="center-align">Digital Ocean</h5>
+            <img src="images/svg/digitalocean.svg" width="100" class="responsive-img skill-images">
+        </div>
+        <div class="col m3 s12">
+            <h5 class="center-align">Vagrant</h5>
+            <img src="images/svg/vagrant.svg" width="100" class="responsive-img skill-images">
+        </div>
+    </div>
+</div>
